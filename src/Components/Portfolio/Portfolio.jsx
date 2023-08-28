ee9541ad5d8e50c4702a28ddb7e0bb825df37808
Ayush Verma <itsmedoraemon19@gmail.com>
2023-08-28 19:37:29 +0530

    fixed

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './Portfolio.css'
-import {Swiper,SwiperSlide} from 'swiper/react'
+import debsoc from '../../img/debsoc.png'
 import vistaar from '../../img/vistaar.png'
 import fic from '../../img/fic.png'
 import kitchen from '../../img/kitchen.png'
-import debsoc from '../../img/debsoc.png'
-import 'swiper/css';
+
 import Slider from 'infinite-react-carousel';
 
 const Portfolio = () => {
@@ -15,30 +14,24 @@ const Portfolio = () => {
         <span>Recent projects</span>
         <span>Portfolio</span>
 
-        {/* slider */}
-        {/* <Swiper
-        spaceBetween={30}
-        slidesPerView={3}
-        grabCursor={true}
-        className='portfolio-slider'> */}
+     
         <Slider className="portfolio-slider"
         autoplay={true}
         autoplayScroll={1}
         autoplaySpeed={2000}>
 
-         {/* <SwiperSlide> */}
             <img src={vistaar} alt="" />
-            {/* </SwiperSlide>   
-            <SwiperSlide> */}
+              
+       
             <img src={fic} alt="" />
-            {/* </SwiperSlide>   
-            <SwiperSlide> */}
+              
+           
             <img src={kitchen} alt="" />
-            {/* </SwiperSlide> 
-            <SwiperSlide> */}
+            
+           
             <img src={debsoc} alt="" />
-            {/* </SwiperSlide>    
-        </Swiper> */}
+               
+     
         </Slider>
     </div>
   )
