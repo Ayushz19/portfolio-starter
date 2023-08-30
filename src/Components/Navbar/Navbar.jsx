cd50898bb3ca14b5acd32ca1607588db24daa52c
Ayush Verma <itsmedoraemon19@gmail.com>
2023-08-31 03:13:20 +0530

    cross button added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,55 +2,86 @@ import React from "react";
 import "./Navbar.css";
 import Toogle from "../Toogle/Toogle";
 import { Link } from "react-scroll";
-import Ham from '../../img/ham.png'
-import { useState } from "react";
+import Ham from "../../img/ham.png";
 
+import cross from '../../img/cross.png'
 
 const Navbar = () => {
   // const [open,setopen]=useState
 
-
   return (
-    <div className="n-wrapper"
-    >
-      
-      <img src={Ham} className="ham" onClick={()=>{
-        document.getElementById("mobnav").classList.toggle("mobnav-vis")
-      }}>
-      
-      </img>
-      
+    <div className="n-wrapper">
+      <img
+        src={Ham}
+        className="ham"
+        onClick={() => {
+          document.getElementById("mobnav").classList.toggle("mobnav-vis");
+        }}
+      ></img>
+
       <div className="n-left">
         <div className="n-name">Ayush </div>
         <Toogle />
       </div>
       <div className="n-right">
         <div className="n-list">
-          <ul style={{ listStyleType: "none" }} id="mobnav">
+          
+          <ul style={{ listStyleType: "none" }} id="mobnav"
+           onClick={() => {
+            document
+              .getElementById("mobnav")
+              .classList.toggle("mobnav-vis");
+          }}>
+            <img src={cross} className="cross" alt="" ></img>
+            
             <Link
               spy={true}
               to="Navbar"
               activeClass="activeClass"
               smooth={true}
             >
-              <li onClick={()=>{
-        document.getElementById("mobnav").classList.toggle("mobnav-vis")
-      }}>Home</li>
+              <li
+                onClick={() => {
+                  document
+                    .getElementById("mobnav")
+                    .classList.toggle("mobnav-vis");
+                }}
+              >
+                Home
+              </li>
             </Link>
             <Link spy={true} to="Services" smooth={true}>
-              <li onClick={()=>{
-        document.getElementById("mobnav").classList.toggle("mobnav-vis")
-      }}>Services</li>
+              <li
+                onClick={() => {
+                  document
+                    .getElementById("mobnav")
+                    .classList.toggle("mobnav-vis");
+                }}
+              >
+                Services
+              </li>
             </Link>
             <Link spy={true} to="Experience" smooth={true}>
-              <li onClick={()=>{
-        document.getElementById("mobnav").classList.toggle("mobnav-vis")
-      }}>Experience</li>
+              <li
+                onClick={() => {
+                  document
+                    .getElementById("mobnav")
+                    .classList.toggle("mobnav-vis");
+                }}
+              >
+                Experience
+              </li>
             </Link>
             <Link spy={true} to="Portfolio" smooth={true}>
-              <li onClick={()=>{
-        document.getElementById("mobnav").classList.toggle("mobnav-vis")
-      }}>PortFolio</li>
+              <li
+                onClick={() => {
+                  document
+                    .getElementById("mobnav")
+                    .classList.toggle("mobnav-vis");
+                }}
+              >
+                PortFolio
+              </li>
             </Link>
             <button className="button n-button">Contact Us</button>
           </ul>
