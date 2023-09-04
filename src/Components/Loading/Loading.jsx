1b2616d0f48d3b9171f012583e1c7bc2d8385c03
Ayush Verma <itsmedoraemon19@gmail.com>
2023-09-05 02:36:32 +0530

    libray

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Loading.css";
-import svg from "../../img/hi.svg";
+
 import ReactVivus from "react-vivus";
-import AtomicSpinner from "atomic-spinner";
+
 
 const Loading = () => {
   return (
